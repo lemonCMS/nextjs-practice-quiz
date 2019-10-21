79a1ac291a09139d87bfeedf28360b571a8b490b
raymond <rsvvuuren@hotmail.com>
2019-10-22 00:29:56 +0200

    remove console log

--- webapp/pages/en.js
+++ webapp/pages/en.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import MultipleChoice from '../components/multiple-choice'
-import Head from "next/head";
 import Container from "@material-ui/core/Container";
 
 class Index extends React.Component {
-
   static async getInitialProps({ctx}) {
     return {
       manual: typeof ctx.query.manual !== 'undefined' && ctx.query.manual === 'true',
@@ -14,13 +12,7 @@ class Index extends React.Component {
 
   render() {
     const {manual, limit} = this.props;
-
     return (<>
-      <Head>
-        <title>Engels oefenen</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      </Head>
       <Container>
         <MultipleChoice question={'en'} answer={'nl'} manual={manual} limit={limit} />
       </Container>
--- webapp/pages/nl.js
+++ webapp/pages/nl.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import MultipleChoice from '../components/multiple-choice'
-import Head from "next/head";
 import Container from "@material-ui/core/Container";
 
 class Index extends React.Component {
 
   render() {
     return (<>
-      <Head>
-        <title>Hailey test</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      </Head>
       <Container>
         <MultipleChoice question={'nl'} answer={'en'} />
       </Container>
